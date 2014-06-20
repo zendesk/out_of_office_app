c127b85958e80595ce1da70e1702d62d0faae013
Dan Pawluk <dpawluk@zendesk.com>
2014-06-20 16:41:21 -0500

    added support for paginating the agent list

--- app.js
+++ app.js
@@ -1,43 +1,85 @@
 (function() {
 
-  return {
-    events: {
-      'app.activated':'init',
-      'getAgentList.done':'drawTemplate'
-    },
+    return {
+        users: [],
 
-    requests: {
-    	getAgentList: function() {
-    		return {
-    			url: '/api/v2/users.json?role[]=agent&role[]=admin'
-    		};
-    	}
-    },
+        events: {
+            'app.activated': 'init'
+        },
 
-    init: function() {
-    	this.ajax('getAgentList');
-    },
+        requests: {
+            getAgentList: function(page) {
+                return {
+                    url: helpers.fmt('/api/v2/users.json?role[]=agent&role[]=admin&page=%@', page)
+                };
+            }
+        },
 
-    drawTemplate: function(data) {
-    	var userlist = _.chain(data.users)
-    	.map(function(user){
-    		return {
-    			id: user.id,
-    			name: user.name,
-    			role: user.role,
-    			tags: user.tags
-    		}
-    	})
-    	.value();
-    	console.log(userlist);
-    	this.switchTo('main', {
-    		userlist: userlist
-    	});
-    },
+        init: function() {
+            this.users = [];
+            var fetchedUsers = this._paginate({
+                request: 'getAgentList',
+                entity: 'users',
+                page: 1
+            });
 
-    logData: function(data) {
-    	console.log(data);
-    }
-  };
+            fetchedUsers
+                .done(_.bind(function(data) {
+                    this.users = data;
+                    this.drawTemplate();
+                }, this))
+                .fail(_.bind(function() {
+                    services.notify("Something went wrong and we couldn't reach the REST API to retrieve all user data", 'error');
+                }, this));
+
+        },
+
+        drawTemplate: function() {
+            var userlist = _.chain(this.users)
+                .map(function(user) {
+                    return {
+                        id: user.id,
+                        name: user.name,
+                        role: user.role,
+                        tags: user.tags
+                    }
+                })
+                .value();
+            this.switchTo('main', {
+                userlist: userlist
+            });
+        },
+        _paginate: function(a) {
+            var results = [];
+            var initialRequest = this.ajax(a.request, a.page);
+            // create and return a promise chain of requests to subsequent pages
+            var allPages = initialRequest.then(function(data) {
+                results.push(data[a.entity]);
+                var nextPages = [];
+                var pageCount = Math.ceil(data.count / 100);
+                for (; pageCount > 1; --pageCount) {
+                    nextPages.push(this.ajax(a.request, pageCount));
+                }
+                return this.when.apply(this, nextPages).then(function() {
+                    var entities = _.chain(arguments)
+                        .flatten()
+                        .filter(function(item) {
+                            return (_.isObject(item) && _.has(item, a.entity));
+                        })
+                        .map(function(item) {
+                            return item[a.entity];
+                        })
+                        .value();
+                    results.push(entities);
+                }).then(function() {
+                    return _.chain(results)
+                        .flatten()
+                        .compact()
+                        .value();
+                });
+            });
+            return allPages;
+        }
+    };
 
 }());
