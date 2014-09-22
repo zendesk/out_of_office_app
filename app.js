ea9b5f2766b898c4cb52167755b35b0380a25861
Ben Evans <bevans@zendesk.com>
2014-09-22 11:58:50 -0500

    Refactored UI, modal actions, modal handling
    - popmodal is now properly generic, and handles bind/unbind of events itself
    - moved verbage out of modal_ui and into main app.js
    - merged all app location code into one ui module
    - minor modifications to ticket sidebar template to remove redundancy

--- app.js
+++ app.js
@@ -10,8 +10,35 @@
 
     events: require('events'), //magic happens here
 
-    requests: require('requests')     
- 
-    };
+    requests: require('requests'),
+
+    changeStatusMessage: function(user) {
+        return { 
+            available: {
+                header:  'Please confirm status change',
+                content: '<p>This action will mark ' + user.name + ' as available and allow tickets to be assigned.</p>',
+                confirm: '<p style="color: white; background-color: #79a21d; border-color: #79a21d; font-size: 100%; height: 100%; line-height: 200%; border-radius: 3px; padding-top: 8px; padding-bottom: 8px">Mark as Available</p>',        
+                cancel:  'Cancel'
+            },
+            unavailable: {
+                header:  'Please confirm status change',
+                content: '<p>This action will reassign ' + user.name + '&#39;s open tickets and change their status to away.</p>',
+                confirm: '<p style="color: white; font-size: 100%; height: 100%; line-height: 200%; border-radius: 3px; padding-top: 8px; padding-bottom: 8px">Mark as Unavailable</p>',
+                cancel:  'Cancel',
+                options: '<input type="checkbox" name="reassign_current" /> Unassign All Open Tickets?'
+            },
+        };
+
+    },
+
+
+    saveHookMessage: function(user) {
+        return {
+            header:  'Assignee is Unavailable',
+            content: '<p>The assignee you have selected: ' + user.name + ' is currently marked as unavailable and cannot have tickets assigned to them.',
+            confirm: '<a href="#/users/' + user.id + '">Go to Agent Profile</a>',
+            cancel:  '<p style="color: white; background-color: #79a21d; border-color: #79a21d; font-size: 100%; height: 100%; line-height: 200%; border-radius: 3px; padding-top: 8px; padding-bottom: 8px">Cancel</p>', 
+        };
+    },
 
 }());
