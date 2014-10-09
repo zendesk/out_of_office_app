77f5fe88abe938f7f0e85006f0bec17e7952797f
Ben Evans <bevans@zendesk.com>
2014-10-09 11:36:14 -0500

    Managing status now working
    - modal now correctly works
    - can now unassign all tickets
    - now refreshes when things are updated via the app
    
    TODO:
        - Trigger
        - Settings
        - Save Hook

--- app.js
+++ app.js
@@ -37,15 +37,36 @@
 
         //app.init, installed_app
         init: function(app) {
+            var settings =  {
+                installed: false,
+                installationID: undefined,
+
+                createTrigger: false,            
+                triggerTitle: 'out-of-office app trigger',
+                triggerID: undefined,
+
+                userFieldName: 'Agent Out?',
+                userFieldKey: 'agent_ooo',
+
+                confirmChange: true,
+
+                unassignTickets:false,            
+                tagUnassignedTickets: false,
+                unassignTag: 'reasign_ooo',
+
+                preventAssignOOO: false,
+            };
             this.require = require('context_loader')(this);
-            var install = this.require('install_app', this.settings);
+            var install = this.require('install_app', settings);
             install.loadSettings();
         },
 
         //loaded_settings
-        createSettings: function(settings) {
-            this.settings = settings;
+        createSettings: function(evt) {
+            this.settings = evt.settings;
             this.trigger("render_app");
+            this.trigger("ticket.save");
+
         },
         
 
@@ -53,7 +74,6 @@
         render: function() {
             var ui = this.require('ui', this.settings);
             if (this.currentLocation() == 'nav_bar') {
-                console.log('navbar');
                 ui.renderNavBar(); //side effect
             } else if (this.currentLocation() == 'user_sidebar') {
                 ui.renderUser(); //side effect
@@ -61,35 +81,64 @@
                 ui.renderTicket();
             }            
         },
-        
+
 
         //click .set-status
         verifyChange: function(evt) {
-            console.log('hello');
             var agentID = evt.currentTarget.value;
+            var that = this;
             if(this.settings.confirmChange) {
-                var modal = this.require('modal_ui', this.settings);
-                modal.popModal(this.settings.changeStatusMessage, ["status_toggle", agentID], ["status_cancel"]);
+                var modal = this.require('popmodal');
+                var that = this;                
+                this.ajax('getSingleAgent', agentID).done(function(agent) {
+                    agent = agent.user;
+                    var message = that.changeStatusMessage(agent.name).unavailable;
+                    if(agent.user_fields.agent_ooo) {
+                        message = that.changeStatusMessage(agent.name).available;
+
+                    }
+                    modal(message, function(options) { 
+                        console.log(unassignTickets);
+                        console.log(options[0]);
+                        var unassignTickets = that.settings.unassignTickets;
+                        if(options[0] === "on") {
+                            unassignTickets = true;
+                        }
+
+                        console.log(unassignTickets);
+                        
+                        that.trigger("toggle_status", {agentID: agentID, unassignTickets: unassignTickets});
+                    });
+                });
             } else {
-                this.trigger("toggle_status", agentID);
+                this.trigger("toggle_status", {agentID: agentID});
             }
         },
-        
+
         //toggle_status
-        updateStatus: function(agentID) {
-            console.log(agentID);
-            console.log('toggleStatus');
+        updateStatus: function(evt) {
+            var agentID = evt.agentID;
+            console.log(evt.unassignTickets);
+            var unassignTickets = evt.unassignTickets;
+        console.log(unassignTickets);
             var that = this;
             this.require('update_status', this.settings).
-                toggleStatus(agentID).done(function(status) {
-                that.trigger('status_changed', status, agentID);
+                toggleStatus(agentID, unassignTickets).done(function(agentID) {
+                //that.trigger('status_changed', {status: status, agentID: agentID});
+            }).fail(function() {
+            that.trigger("render_app");
+                
             });
         },
 
 
         //status_changed
-        notifyStatus: function(status, name) {
-            services.notify("Updated status for " + name + " to " + status + ".");
+        notifyStatus: function(evt) {
+            var status = "here"
+            if(evt.agent.user_fields.agent_ooo) {
+                status = "away";
+            }
+            services.notify("Updated status for " + evt.agent.name + " to " + status + ".");
         },
         
         //unassigned_ooo
@@ -126,17 +175,17 @@
             });
         },
     
-        changeStatusMessage: function(user) {
+        changeStatusMessage: function(name) {
             return { 
                 available: {
                     header:  'Please confirm status change',
-                    content: '<p>This action will mark ' + user.name + ' as available and allow tickets to be assigned.</p>',
+                    content: '<p>This action will mark ' + name + ' as available and allow tickets to be assigned.</p>',
                     confirm: '<p style="color: white; background-color: #79a21d; border-color: #79a21d; font-size: 100%; height: 100%; line-height: 200%; border-radius: 3px; padding-top: 8px; padding-bottom: 8px">Mark as Available</p>',        
                     cancel:  'Cancel'
                 },
                 unavailable: {
                     header:  'Please confirm status change',
-                    content: '<p>This action will reassign ' + user.name + '&#39;s open tickets and change their status to away.</p>',
+                    content: '<p>This action will reassign ' + name + '&#39;s open tickets and change their status to away.</p>',
                     confirm: '<p style="color: white; font-size: 100%; height: 100%; line-height: 200%; border-radius: 3px; padding-top: 8px; padding-bottom: 8px">Mark as Unavailable</p>',
                     cancel:  'Cancel',
                     options: '<input type="checkbox" name="reassign_current" /> Unassign All Open Tickets?'
