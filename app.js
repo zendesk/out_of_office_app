b77a8c4dabae4a52c60d513346ab1d1d850ee9ea
Ben Evans <bevans@zendesk.com>
2014-12-05 04:02:37 -0600

    Merged changes, some additons
    - more moving of strings
    - starting to merge in changes from Master

--- app.js
+++ app.js
@@ -4,28 +4,36 @@
 
         events: require('events'),
 
-        requests: require('requests'),
+        requests: {},
 
         options:  {
             appTitle: 'ooo_app',
-            installed: false,
+            requirementsCreated: false,
             installationID: undefined,
 
             createTrigger: true,
             triggerTitle: 'out-of-office app unassign trigger [System Trigger]',
-            triggerID: undefined,
-
+            userFieldDescription: "This field was created by the out-of-office app. Don't delete it, unless you want everything to break",
             userFieldName: 'Agent Out?',
             userFieldKey: 'agent_ooo',
-
             confirmChange: true,
-
-            unassignTickets:false,
-
+            unassignTickets: false,
             preventAssignOOO: true,
 
-            changeStatusMessage: function(name) {
-                return { 
+            // The following is not used in the OOO app due to other apps modal collision issues and will be saved for use in a future release
+            // saveWarningButton: warning = '<p style="margin-top: 16px; margin-bottom: 10px; margin-left: 60px; font-weight: bold; font-size: 14px;">AGENT UNAVAILABLE</p><p class="btn" style="width: 250px; font-weight: bold; font-size: 14px; margin-bottom: 16px; padding-top: 10px; padding-bottom: 10px;" onclick="$(\'button.status-toggle\').trigger(\'click\');">Update ' + agent.name + '\'s status</p>';
+            
+            saveFailMessage: '<p style="margin-top: 16px; margin-bottom: 10px; text-align: center; font-weight: bold; font-size: 20px;">TICKET NOT SAVED</p>',
+            saveWarning: function(name) { 
+                return '<p style="margin-top: 16px; margin-bottom: 10px; text-align: center; font-size: 14px;"><strong>' + name + '</strong> is <strong>UNAVAILABLE</strong><br/></p><p style="margin-top: 12px; margin-bottom: 6px; text-align: center; font-size: 14px;">If this request requires immediate attention please re-assign to a different agent</p>';
+            },
+
+            changeStatusMessage: function(name, unassignTickets) { // right before 127 in ui.js it passes in the agent name and gets an object that contains the available / unavailable html defninitation to create the modal - need to change this function needs another parameter to see maybe where there's availabe & an object - unavailbale & an object is a checkbox in the option section - clear it out to be blank it will no longer send hte checkbox through
+                var checkbox = '<p style="font-family: proxima-nova, sans-serif;"><label><input type="checkbox" name="reassign_current" /><span id="foo">Unassign All Open Tickets</span></label></p>';
+                if (unassignTickets === true) {  // **NOTE** checkbox CHECKED = checkbox HIDDEN ; unassignTickets option TRUE = checkbox HIDDEN
+                    checkbox = undefined;
+                }
+                return {
                     available: {
                         header:  'Please confirm status change',
                         content: '<p>This action will tag <strong>' + name + '</strong> as available and allow tickets to be assigned.</p>',
@@ -37,8 +45,8 @@
                         content: '<p>This action will tag <strong>' + name + '</strong> as out of office and prevent tickets from being assigned.</p>',
                         confirm: '<p style="color: white; font-family: proxima-nova, sans-serif; font-size: 100%; height: 100%; line-height: 200%; border-radius: 3px; padding-top: 8px; padding-bottom:8px">Set to Unavailable</p>',
                         cancel:  'Cancel',
-                        options: '<p style="font-family: proxima-nova, sans-serif;"><label><input type="checkbox" name="reassign_current" /><span id="foo">Unassign All Open Tickets</span></label></p>'
-                    },
+                        options: checkbox
+                    }
                 };
             },
             lockRender: false,
@@ -50,7 +58,7 @@
         init: function(app) {
             this.switchTo('loading');
             this.require = require('context_loader')(this);            
-            this.require('install_app', this.options)();
+            this.require('install_app', this.options).loadSettings();
         },
 
         //loaded_settings
@@ -95,7 +103,7 @@
             var agentID = evt.agentID;
             var unassignTickets = evt.unassignTickets;
             var that = this;
-            this.require('update_status', this.options)(agentID, unassignTickets);
+            this.require('update_status', this.options).toggleStatus(agentID, unassignTickets);
         },
 
         //status_changed
@@ -110,6 +118,12 @@
             var tagsMessage = '<p>Tickets assigned to <strong>' + evt.agent.name + '</strong> with the status <strong>Pending/On-Hold</strong> will have the <strong>\"agent_ooo\"</strong> tag <strong>' + tags + '</strong>.</p>';
             services.notify(statusMessage + tagsMessage, 5000);
             this.trigger("render_app");
+        },
+
+        warnStatus: function(evt) {
+            if(evt.agent.user_fields.agent_ooo) {
+                services.notify(this.options.saveWarning(evt.agent.name), 'warning', 5000);
+            }
         },
 
         //status_error
@@ -136,7 +150,7 @@
                 status = ' Pending/On-Hold tickets with the agent status for ';
             } else if(evt.ticketView == 'ticketPreview') {
                 action = 'Unassigned ';
-                status = ' tickets previously assigned to ';
+                status = ' Open tickets previously assigned to ';
             }
             services.notify(action + evt.count + status + evt.name + ".");
         },
@@ -150,25 +164,31 @@
             var ui = this.require('ui', this.options);
             return ui.renderSave();        
         },
+
+        //functional_error
+        functionalError: function(evt) {
+            console.log(evt);
+            switch(evt.location) {
+                case 'setStatusPending': services.notify('Unable to look up pending tickets for ' + evt.agent.name + '. This agent may not be an assignable agent.', 'error', 5000);
+                    break;
+                case 'setStatus': if(evt.errorCode == 403) {
+                    services.notify('Permissions error while updating status for ' + evt.agent.name + '. Please make sure you are allowed to update their user profile.', 'error', 5000);
+                }
+            }
+        },
+
+        //network_error
+        networkError: function(evt) {
+            console.log(evt);
+        },
         
         //getAllAgents.fail
         //getSingleAgent.fail
-        //url.fail
-        //setAgentStatus.fail
-        //getTriggerData.fail
-        //modifyTrigger.fail
-        //unassignMany.fail
-        //ticketPreview.fail
-        //getSingleTicket.fail
-        //createTrigger.fail
-        //createUserField.fail
-        //getInstalledApps.fail
-        notifyError: function(string) {
+        renderRetry: function() {
             if(this.renderRetries < 0){
                 this.trigger("render_app");
             }
             this.renderRetries++;
-            console.log("Error: Unable to " + string, 'error');
         }
     };
 }());
