47ce2d0516450d2cb90b7d386fd879e0a2bde26d
Ben Evans <bevans@zendesk.com>
2014-12-09 21:09:22 -0600

    removed unused methods, more comments

--- app.js
+++ app.js
@@ -65,24 +65,24 @@
         //toggle_status
         updateStatus: function(evt) {
             var agentID = evt.agentID;           
-            var unassignTickets = evt.unassignTickets;  //agent ID and prefrence on wheter to unasign open tickets will be passed in
-            this.require('update_status', this.options).toggleStatus(agentID, unassignTickets);
+            var unassignTickets = evt.unassignTickets;  //agent ID and prefrence on whether to unasign open tickets will be passed in
+            this.require('update_status', this.options).toggleStatus(agentID, unassignTickets); //here we directly call in a function from the update_status.js module - this will handle the actual server-side change as well as tagging all tickets
         },
 
             // The following is not used in the OOO app due to other apps modal collision issues and will be saved for use in a future release
             // saveWarningButton: warning = '<p style="margin-top: 16px; margin-bottom: 10px; margin-left: 60px; font-weight: bold; font-size: 14px;">AGENT UNAVAILABLE</p><p class="btn" style="width: 250px; font-weight: bold; font-size: 14px; margin-bottom: 16px; padding-top: 10px; padding-bottom: 10px;" onclick="$(\'button.status-toggle\').trigger(\'click\');">Update ' + agent.name + '\'s status</p>';
 
-        saveFailMessage: function() {
+        saveFailMessage: function() {   //this is located here to format messaging that may be used in either ui.js or further down in app.js
             var failMessage = this.I18n.t('saveFailMessage');
             return '<p style="margin-top: 16px; margin-bottom: 10px; text-align: center; font-weight: bold; font-size: 20px;">' + failMessage + '</p>';
         },
-        saveWarningMessage: function(name) { 
+        saveWarningMessage: function(name) {  //TODO: rework current strings to use placeholders instead of concatenation
             var warnStatus  = this.I18n.t('saveWarningMessage.status'),
                 warnMessage = this.I18n.t('saveWarningMessage.message');
             return '<p style="margin-top: 16px; margin-bottom: 10px; text-align: center; font-size: 14px;"><strong>' + name + '</strong> is <strong>' + warnStatus + '</strong><br/></p><p style="margin-top: 12px; margin-bottom: 6px; text-align: center; font-size: 14px;">' + warnMessage + '</p>';
         },
 
-        changeStatusMessage: function(name, unassignTickets) { // right before 127 in ui.js it passes in the agent name and gets an object that contains the available / unavailable html defninitation to create the modal - need to change this function needs another parameter to see maybe where there's availabe & an object - unavailbale & an object is a checkbox in the option section - clear it out to be blank it will no longer send hte checkbox through
+        changeStatusMessage: function(name, unassignTickets) { //this generates an object which conforms to the definition needed by popmodal.js
             var availableHeader             =   this.I18n.t('changeStatusMessage.available.header'),
                 availableContentFirst       =   this.I18n.t('changeStatusMessage.available.content.first'),
                 availableContentSecond      =   this.I18n.t('changeStatusMessage.available.content.second'),
@@ -97,14 +97,13 @@
 
             var checkbox = '<p style="font-family: proxima-nova, sans-serif;"><label><input type="checkbox" name="reassign_current" /><span id="foo">' + checkboxText + '</span></label></p>';
             if (unassignTickets === true) {  // **NOTE** checkbox CHECKED = checkbox HIDDEN ; unassignTickets option TRUE = checkbox HIDDEN
-                checkbox = undefined;
+                checkbox = undefined; //if the checkbox is undefined it will not show up in the modal
             }
-            if (!this.options.preventAssignOOO) {
+            if (!this.options.preventAssignOOO) { //slightly different verbage if we aren't going to prevent you from assigning tickets
                 unavailableContentSecond    =   this.I18n.t('changeStatusMessage.unavailable.content.secondA');
                 availableContentSecond      =   this.I18n.t('changeStatusMessage.available.content.secondA');
             }
             return {
-
                 available: {
                     header:  availableHeader,
                     content: '<p>' + availableContentFirst + '<strong>' + name + '</strong>' + availableContentSecond + '</p>',
@@ -121,45 +120,23 @@
             };
         },
 
-        //status_changed
-        notifyStatus: function(evt) {
-            var status = this.I18n.t('notify.status.available');
+        //status_changed                //TODO: unify status/sysem warnings into less functions
+        notifyStatus: function(evt) {   //this generates the services.notify messages when an agent's status is updated
+            var status = this.I18n.t('notify.status.available');        
             var tags = this.I18n.t('notify.status.tags.removed');
-            if(evt.agent.user_fields[this.options.userFieldKey]) {
-                status = this.I18n.t('notify.status.unavailable');
+            if(evt.agent.user_fields[this.options.userFieldKey]) { //there are several options for what might be notified about
+                status = this.I18n.t('notify.status.unavailable'); //this sets the message to warn that the agent is being set to unavailable
                 tags = this.I18n.t('notify.status.tags.added');
             } 
-            var statusMessage = '<p>' + evt.agent.name + this.I18n.t('notify.status.statusMessage') + status + '</p>';
+            var statusMessage = '<p>' + evt.agent.name + this.I18n.t('notify.status.statusMessage') + status + '</p>'; //this message is used to confirm that the tickets are updated
             var tagsMessage = '<p>' + this.I18n.t('notify.status.tagsMessage.one') + evt.agent.name + this.I18n.t('notify.status.tagsMessage.two') + tags + '</p>';
-            services.notify(statusMessage + tagsMessage, 5000);
-            this.trigger("render_app");
-        },
-
-        warnStatus: function(evt) {
-            if(evt.agent.user_fields[this.options.userFieldKey]) {
-                services.notify(this.saveWarningMessage(evt.agent.name), 'warning', 5000);
-            }
-        },
-
-        //status_error
-        notifyFail: function(evt) {
-            services.notify(this.I18n.t('notify.fail') + evt.agent.name, 'alert');
-        },
-
-        //createTrigger.done
-        //createUserField.done
-        notifyInstalled: function(item) {
-            switch(item) {
-                case 'trigger': services.notify(this.I18n.t('notify.installed.trigger'), 'alert');
-                    break;
-                case 'user_field': services.notify(this.I18n.t('notify.installed.user_field'), 'alert');
-                    break;
-            }
+            services.notify(statusMessage + tagsMessage, 5000); //actually send the message
+            this.trigger("render_app"); //since the agent's status will have changed, this calls .render() and causes the UI to be updated
         },
 
         //tickets_tagged
-        notifyUnAssign: function(evt) {
-            var action = this.I18n.t('notify.unassign.default.action');
+        notifyUnAssign: function(evt) { //the name here is a bit of a misnomer now - while it is fired when open tickets are tagged and then unassigned,
+            var action = this.I18n.t('notify.unassign.default.action'); //it is also used when pending/on-hold tickets are tagged
             var status = this.I18n.t('notify.unassign.default.status');
             if(evt.ticketView == 'pendingTickets') {
                 status = this.I18n.t('notify.unassign.pendingTickets.status');
@@ -170,15 +147,29 @@
             services.notify(action + evt.count + status + evt.name + ".");
         },
 
-        //assigned_ooo
-        notifyAssign: function(name) {
-            services.notify(this.I18n.t('notify.assign.one') + name + this.I18n.t('notify.assign.two'), 'alert');
+
+        //update_warning
+        warnStatus: function(evt) { //this will show a warning if a ticket is saved and the agent assigned is out of office
+            if(evt.agent.user_fields[this.options.userFieldKey]) {
+                services.notify(this.saveWarningMessage(evt.agent.name), 'warning', 5000);
+            }
+        },
+
+        //createTrigger.done
+        //createUserField.done
+        notifyInstalled: function(item) { //this notifys when the required triggers/fields are created
+            switch(item) {
+                case 'trigger': services.notify(this.I18n.t('notify.installed.trigger'), 'alert');
+                    break;
+                case 'user_field': services.notify(this.I18n.t('notify.installed.user_field'), 'alert');
+                    break;
+            }
         },
 
         //functional_error
-        functionalError: function(evt) {
-            console.log(evt);
-            switch(evt.location) {
+        functionalError: function(evt) { //functional_error events are fired throughought the app when something is interrupted or a request fails. 
+            console.log(evt);            //we've made an effort to attach useful debugging information to those events - they show up in the console and may be helpful if errors to occur
+            switch(evt.location) {       //this also allows us to catch *expected* errors and give a slightly more helpful message
                 case 'setStatusPending': services.notify(this.I18n.t('functionalError.setStatusPending.one') + evt.agent.name + this.I18n.t('functionalError.setStatusPending.two'), 'error', 5000);
                     break;
                 case 'setStatus': if(evt.errorCode == 403) {
@@ -188,14 +179,14 @@
         },
 
         //network_error
-        networkError: function(evt) {
-            console.log(evt);
+        networkError: function(evt) {  //network errors (failures of ajax requests) are fired as well, with more low-level debugging information
+            console.log(evt);          //between functional_error and network_error the goal is to expose parts of the app state that might be helpful for debugging
         },
         
         //getAllAgents.fail
         //getSingleAgent.fail
-        renderRetry: function() {
-            if(this.renderRetries < 0){
+        renderRetry: function() {       //in some limited situations, we might be able to recover from a failed network request.
+            if(this.renderRetries < 0){     //this will retry the events that are typically used in .render() once in case the network error was just a transient failure
                 this.trigger("render_app");
             }
             this.renderRetries++;
