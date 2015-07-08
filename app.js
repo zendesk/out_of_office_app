79f6cd8e930765dd128cbe3ca8c07b0df76d7405
Ed Pritchard <epritchard83@gmail.com>
2015-07-08 14:13:31 -0500

    Adding check for app visibility areas
    - Add check for ticket sidebar visibility
    - Add check for user sidebar visibility
    - Add translation strings for new setting in EN and DE

--- app.js
+++ app.js
@@ -18,10 +18,10 @@
         },
 
         lockRender: false, //this is used to prevent the UI from rendering twice due to Lotus bug #885042
-        renderRetries: 0,  //in some instances we will retry a failed network request, this tracks that to limit it to a few retries      
+        renderRetries: 0,  //in some instances we will retry a failed network request, this tracks that to limit it to a few retries
 
         //app.created
-        init: function(app) {     
+        init: function(app) {
             this.switchTo('loading');
             this.require = require('context_loader')(this);            //this is a helper for passing context between common.js modules
             this.require('install_app', this.options).loadSettings();  //on app creation, check to make sure requirements are present, load settings into memory
@@ -36,17 +36,19 @@
             var ui = this.require('ui', this.options);  //load in the ui.js module which owns most of the methods which access the DOM
             if(!this.lockRender) {                          //check to see if rendering is prevented
                 if (this.currentLocation() == 'nav_bar') {
-                    ui.renderNavBar();                  
-                } else if (this.currentLocation() == 'user_sidebar') {
-                    ui.renderUser(); 
-                } else if (this.currentLocation() == 'ticket_sidebar' || this.currentLocation() == 'new_ticket_sidebar') {
+                    ui.renderNavBar();
+                } else if (this.currentLocation() == 'user_sidebar' && this.options.userSidebarVisibility) {
+                    ui.renderUser();
+                } else if ((this.currentLocation() == 'ticket_sidebar' || this.currentLocation() == 'new_ticket_sidebar') && this.options.ticketSidebarVisibility) {
                     ui.renderTicket();
-                } 
-            } 
+                } else {
+                    this.hide();
+                }
+            }
         },
 
         //click .status-toggle
-        verifyChange: function(evt) {              
+        verifyChange: function(evt) {
             var agentID = evt.currentTarget.value;      //this is set in the button html/template and will be the ID of the agent being modified
             var ui = this.require('ui', this.options);  //load ui.js module
             var that = this;                            //clunky - TODO: refactor
@@ -54,7 +56,7 @@
                 ui.renderStatusModal(agentID);          //this uses the UI method to generate a modal to confirm the agent status change
             } else {
                 this.trigger("toggle_status", {agentID: agentID, unassignTickets: that.options.unassignTickets}); //trigger a change of status
-            }               //most functions in app.js are on an event hook - while the various modules could call them directly, 
+            }               //most functions in app.js are on an event hook - while the various modules could call them directly,
                             //in some instances this makes keeping things encapsulated easier
                             //could just call this.render() here, but being consistent about how things are accessed...
                             //would be interesting to go back and see if this organization method actually helps or not - too late to change it now really
@@ -62,7 +64,7 @@
 
         //toggle_status
         updateStatus: function(evt) {
-            var agentID = evt.agentID;           
+            var agentID = evt.agentID;
             var unassignTickets = evt.unassignTickets;  //agent ID and prefrence on whether to unasign open tickets will be passed in
             this.require('update_status', this.options).toggleStatus(agentID, unassignTickets); //here we directly call in a function from the update_status.js module - this will handle the actual server-side change as well as tagging all tickets
         },
@@ -106,7 +108,7 @@
                 available: {
                     header:  availableHeader,
                     content: '<p>' + availableContentFirst + '<strong>' + name + '</strong>' + availableContentSecond + '</p>',
-                    confirm: '<p style="color: white; font-family: proxima-nova, sans-serif; background-color: #79a21d; border-color: #79a21d; font-size: 100%; height: 100%; line-height: 200%; border-radius: 3px; padding-top: 8px; padding-bottom: 8px">' + availableConfirm + '</p>',        
+                    confirm: '<p style="color: white; font-family: proxima-nova, sans-serif; background-color: #79a21d; border-color: #79a21d; font-size: 100%; height: 100%; line-height: 200%; border-radius: 3px; padding-top: 8px; padding-bottom: 8px">' + availableConfirm + '</p>',
                     cancel:  availableCancel
                 },
                 unavailable: {
@@ -121,12 +123,12 @@
 
         //status_changed                //TODO: unify status/sysem warnings into less functions
         notifyStatus: function(evt) {   //this generates the services.notify messages when an agent's status is updated
-            var status = this.I18n.t('notify.status.available');        
+            var status = this.I18n.t('notify.status.available');
             var tags = this.I18n.t('notify.status.tags.removed');
             if(evt.agent.user_fields[this.options.userFieldKey]) { //there are several options for what might be notified about
                 status = this.I18n.t('notify.status.unavailable'); //this sets the message to warn that the agent is being set to unavailable
                 tags = this.I18n.t('notify.status.tags.added');
-            } 
+            }
             var statusMessage = '<p>' + evt.agent.name + this.I18n.t('notify.status.statusMessage') + status + '</p>'; //this message is used to confirm that the tickets are updated
             var tagsMessage = '<p>' + this.I18n.t('notify.status.tagsMessage.one') + evt.agent.name + this.I18n.t('notify.status.tagsMessage.two') + tags + '</p>';
             services.notify(statusMessage + tagsMessage, 'alert'); //actually send the message
@@ -167,18 +169,18 @@
         },
 
         //functional_error
-        functionalError: function(evt) { //functional_error events are fired throughought the app when something is interrupted or a request fails. 
+        functionalError: function(evt) { //functional_error events are fired throughought the app when something is interrupted or a request fails.
             console.log(evt);            //we've made an effort to attach useful debugging information to those events - they show up in the console and may be helpful if errors to occur
             switch(evt.location) {       //this also allows us to catch *expected* errors and give a slightly more helpful message
                 case 'applyTag': if(evt.errorCode == 429) {
                         services.notify(this.I18n.t('functionalError.ticketUpdateLimit.one') + evt.agent.name + this.I18n.t('functionalError.ticketUpdateLimit.two'), 'error');
-                    } else { 
+                    } else {
                         services.notify(this.I18n.t('functionalError.setStatusPending.one') + evt.agent.name + this.I18n.t('functionalError.setStatusPending.two'), 'error');
                     } break;
                 case 'setStatus': if(evt.errorCode == 403) {
                     services.notify(this.I18n.t('functionalError.setStatus.one') + evt.agent.name + this.I18n.t('functionalError.setStatus.two'), 'error');
                 } else if(evt.errorCode == 422) {
-                    services.notify(this.I18n.t('functionalError.setStatus.one') + evt.agent.name + this.I18n.t('functionalError.setStatus.two'), 'error');   
+                    services.notify(this.I18n.t('functionalError.setStatus.one') + evt.agent.name + this.I18n.t('functionalError.setStatus.two'), 'error');
                 } break;
             }
             console.log(evt.location);
@@ -190,7 +192,7 @@
             console.log(evt);          //between functional_error and network_error the goal is to expose parts of the app state that might be helpful for debugging
             this.renderRetry();
         },
-        
+
         //getAllAgents.fail
         //getSingleAgent.fail
         renderRetry: function() {       //in some limited situations, we might be able to recover from a failed network request.
